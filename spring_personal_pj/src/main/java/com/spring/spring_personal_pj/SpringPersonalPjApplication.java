570cfa693f9b56dc6f560e9d0906f43d359c0d41
Shin Ihyeon <mashin2002@naver.com>
2023-07-25 20:10:33 +0900

    [ADD]스프링 프로젝트

---
+++ spring_personal_pj/src/main/java/com/spring/spring_personal_pj/SpringPersonalPjApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_personal_pj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonalPjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPersonalPjApplication.class, args);
+	}
+
+}
